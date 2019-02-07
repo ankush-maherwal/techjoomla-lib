ffe6d8a434188ad6d9e3e70e5ad87c42085e1582
“ankush_m” <ankush_m@techjoomla.com>
2019-02-07 17:20:55 +0530

    Task #139640 feat: Migrate data of users from old stores as vendors in TJ-Vendors

--- src/techjoomla/sql/updates/mysql/1.1.1.sql
+++ src/techjoomla/sql/updates/mysql/1.1.1.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `#__tj_houseKeeping` (
   `title` varchar(100) NOT NULL COMMENT 'The descriptive title for the housekeeping task',
   `client` varchar(50) NOT NULL COMMENT 'Client extension name',
   `version` varchar(11) NOT NULL COMMENT 'Version for housekeeping task',
-  `state` tinyint(3) NOT NULL DEFAULT 0,
+  `status` tinyint(3) NOT NULL DEFAULT 0,
   `lastExecutedOn` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `params` text NOT NULL,
   PRIMARY KEY (`id`)
